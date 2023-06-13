c545db0b6fc8a0d05eacf0c4ef5b0b025eefbc7b
Jakub <jakuboksa@gmail.com>
2023-06-13 20:41:48 +0200

    Adapt project for deployment to Vercel

--- database/connect.ts
+++ database/connect.ts
@@ -2,22 +2,31 @@ import 'server-only';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-if (!process.env.FLY_IO) config();
+if (!process.env.POSTGRES_URL) {
+  config();
+}
 
 declare module globalThis {
   let postgresSqlClient: ReturnType<typeof postgres> | undefined;
 }
 
 // Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
   if (!globalThis.postgresSqlClient) {
     globalThis.postgresSqlClient = postgres({
+      host: process.env.POSTGRES_HOST || process.env.PG_HOST,
+      username: process.env.POSTGRES_USER || process.env.PGUSERNAME,
+      password: process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD,
+      database: process.env.POSTGRES_DATABASE || process.env.PGDATABASE,
+      ssl: !!process.env.POSTGRES_URL,
       transform: {
         ...postgres.camel,
         undefined: null,
       },
     });
   }
+
   return globalThis.postgresSqlClient;
 }
 
